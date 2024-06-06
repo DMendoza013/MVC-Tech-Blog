2ecba526a72282539b7a1272d7399f838cbdd8dd
DMendoza013 <damien.mendoza013@gmail.com>
2024-06-06 14:03:48 -0700

    updated routes, models, views, and packages. Added public folder with css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 
 router.get('/', async ( req, res ) => {
-
+    res.render('homepage')
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +17,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
