f1b6c7e0dbe3f99daea8827f6db742b2f19180f0
DMendoza013 <damien.mendoza013@gmail.com>
2024-06-10 14:43:57 -0700

    created new views and added routes to them

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,4 +5,17 @@ router.get('/', async ( req, res ) => {
     res.render('homepage')
 });
 
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/homepage');
+      return;
+    }
+    res.render('login');
+  });
+
+
 module.exports = router;
